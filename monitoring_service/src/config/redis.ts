9bcf87a0d5978463a83c1a05c79d019f3f8e6b79
Bikash Mishra <mishrabikash202@gmail.com>
2025-05-12 02:26:27 +0530

    logs added

--- monitoring_service/src/config/redis.ts
+++ monitoring_service/src/config/redis.ts
@@ -7,12 +7,14 @@ const redisClient = createClient({ url: process.env.REDIS_URL });
 
 
 redisClient.on("error", (error) => {
-  console.log("🚀 ~ redisClient.on ~ error:", error)
+ 
   console.log("ERROR WHILE CONNECTING REDIS 🔴🔴");
 });
 
 (async () => {
   redisClient.connect();
+  console.log("Redis connected 🟢🟢 ");
+  
 })();
 
 export { redisClient };
--- monitoring_service/src/config/utils.ts
+++ monitoring_service/src/config/utils.ts
@@ -9,13 +9,13 @@ export async function checkUrl(domain: any, url: string) {
   let finalData: NetworkCheckResponse = {
     status: "unknown",
   };
-  console.log(`Chekcing for ${domain} url: ${url} 🟢🟢`);
+  
   try {
     const httpRes = await axios.get(url);
 
     finalData["status"] = httpRes.status.toString();
   } catch (error: any) {
-    console.log("🚀 ~ checkUrl ~ error:", error);
+   
     finalData.status = "unknown";
   }
 
