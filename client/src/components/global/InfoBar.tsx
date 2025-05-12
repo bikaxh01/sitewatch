c42cc00f56ade448688f513703ee8094f1b20a8b
Bikash Mishra <mishrabikash202@gmail.com>
2025-05-12 16:31:15 +0530

    workflow updated

--- client/src/components/global/InfoBar.tsx
+++ client/src/components/global/InfoBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function InfoBar() {
   return (
-    <div className=" w-full h-fit bg-better-stack-bg text-xs flex items-center justify-center  z-30">
+    <div className=" w-full h-fit bg-better-stack-bg text-xs flex items-center justify-center  z-30 ">
       Note: "Website Under Construction"
     </div>
   );
