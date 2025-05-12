21dcf61d51bdb658d6dbf33719f22dc637a2eab3
Bicky yadav <ybicky86@gmail.com>
2025-05-12 20:46:55 +0530

    ser clie connection establish

--- pub-sub/src/index.ts
+++ pub-sub/src/index.ts
@@ -1,15 +1,23 @@
 import { logger } from "./logger";
 import WebSocket, { WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: 8000 });
 
 wss.on("connection", function connection(ws) {
   ws.on("error", console.error);
 
-  //@ts-ignore
   logger.info("User connected");
 
+  // const pingInterval = setInterval(() => {
+  //   console.log("Sending ping to client");
+  //   ws.send("ping");
+  // }, 3000);
+
   ws.on("message", function message(data, isBinary) {
+    if (data.toString() === "ping") {
+      ws.send("pong");
+    }
+
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data, { binary: isBinary });
